f78d761e563923ecc4a7bc0354077db158216d12
Isaac Yeang <isaacy13@icloud.com>
2026-02-07 20:43:42 -0600

    fix version requirements and add to gitignore

--- Package.swift
+++ Package.swift
@@ -7,8 +7,7 @@ let package = Package(
     name: "swiftly-thinking",
     platforms: [
         .iOS(.v26),
-        .macOS(.v15),
-        .visionOS(.v26),
+        .macOS(.v26)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
